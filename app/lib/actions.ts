13f3ed25df39eb117e88322f152859f72ca4557d
Liinares <miguellinares908@gmail.com>
2024-06-20 20:19:51 +0200

    añadiendo autenticación

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod'
 import { sql } from '@vercel/postgres'
 import { redirect } from 'next/navigation'
 import { revalidatePath } from 'next/cache'
+import { signIn } from '@/auth'
+import { AuthError } from 'next-auth'
 
 const CreateInvoiceSchema = z.object({
   id: z.string(),
@@ -64,4 +66,23 @@ export async function createInvoice(formData: FormData) {
 
   revalidatePath('/dashboard/invoices')
   redirect('/dashboard/invoices')
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }

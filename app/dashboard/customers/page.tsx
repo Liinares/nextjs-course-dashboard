eb8c627affaafc02b8081d1a87948cb20be1a259
Liinares <miguellinares908@gmail.com>
2024-06-24 11:47:17 +0200

    añadiendo info del usuario registrado en la página de customer

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,8 @@
-export default function Page() {
-    return <p>Customers page</p>
+import { auth } from "@/auth"
+
+export default async function Page() {
+    const session = await auth()
+    console.log(session)
+
+    return <p>Customers page {session?.user?.email}</p>
 }
